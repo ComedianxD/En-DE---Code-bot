abda1a9fd62f25c25406e6ea65a29e20f2b71b66
ComedianxD <61299112+ComedianxD@users.noreply.github.com>
2020-11-08 18:07:30 +0200

    Changed encryption algorithm to use the AES cypher

--- bot.py
+++ bot.py
@@ -1,44 +1,62 @@
+import hashlib
 from discord.ext import commands
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+
 
 bot = commands.Bot(command_prefix="+")
 
-file = open("file.txt","r")
-f = file.readlines()
-dic = {} 
-dicc = {}
-for line in f:
-	dicc[line[2]] = line[0]
-	dic[line[0]] = line[2]
 
-@bot.command(name="encode")
-async def fun(ctx,*args):
-	output = ""
-	for arg in args:
-		for char in arg:
-			output = output + dic[char]
-		output = output + " "
-	await ctx.channel.send(output)
+def iv_16(txt):
+	if len(txt) > 16:
+		txt = txt[:16]
+	elif len(txt) < 16:
+		while len(txt) != 16:
+			txt += " "
+	return txt
 
 
-
-@bot.command(name="decode")
-async def fun(ctx,*args):
-	output = ""
-	for arg in args:
-		for char in arg:
-			output = output + dicc[char]
-		output = output + " "
-	await ctx.channel.send(output)
-
-
-#print(dic) 
 @bot.command()
+async def encode(ctx, *, message):
+	key = str(ctx.message.author).encode()
+	key_256 = hashlib.sha256(key).digest()
+	iv = bytes(iv_16(key))
+
+	cipher = AES.new(key_256, AES.MODE_CBC, iv)
+
+	padded_message = pad(message.encode(), AES.block_size)
+
+	encrypted_message = cipher.encrypt(padded_message)
+
+	await ctx.channel.send(encrypted_message)
+
+
+@bot.command()
+async def decode(ctx, message, key):
+	try:
+		key_ = key.encode()
+		key_256 = hashlib.sha256(key_).digest()
+		iv = bytes(iv_16(key_))
+
+		message = eval(message)
+
+		cipher = AES.new(key_256, AES.MODE_CBC, iv)
+
+		decrypted_message = unpad(cipher.decrypt(message), AES.block_size).decode()
+	except ValueError:
+		decrypted_message = "Wrong Key!"
+
+	await ctx.channel.send(decrypted_message)
+
+
+'''@bot.command()
 async def print(ctx, *args):
 	response = ""
 	for arg in args:
 		response = response + " " + arg
 
-	await ctx.channel.send(response)
+	await ctx.channel.send(response)'''
 
-
-#bot.run('NzczMTM3MDU3Mjg3MzcyODEw.X6E1_g.ruYswAyswmTn9a2XgXtyCPIbOWk')
+bot.run('Nzc0OTYzMzA2NDE3ODgxMTE4.X6fa0g.XaSM1HmCW6N50AeykxxkEelWu6Q')
+# bot.run('NzczMTM3MDU3Mjg3MzcyODEw.X6E1_g.ruYswAyswmTn9a2XgXtyCPIbOWk')
